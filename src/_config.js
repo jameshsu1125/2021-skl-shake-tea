2a16bd617486243917d01162836bb4fc1a23885b
jameshsu1125 <70932507+jameshsu1125@users.noreply.github.com>
2021-05-08 10:37:10 +0800

    Add current yt-id on school page

--- src/_config.js
+++ src/_config.js
@@ -23,8 +23,8 @@ module.exports = {
 		'yt-id': 'V_L5nLbZmOc',
 	},
 	school: [
-		{ 'yt-id': 'vXe2tUs5r88', title: '別把保險當存錢!最適合新鮮社畜的保險規劃', tag: '保險新手筆記' },
-		{ 'yt-id': 'bmZXyzAZuIU', title: '別再沈溺蜜月期！新婚小夫妻如何細水長流？', tag: '保險新手筆記' },
+		{ 'yt-id': 'QSNfM_T43D4', title: '別把保險當存錢!最適合新鮮社畜的保險規劃', tag: '保險新手筆記' },
+		{ 'yt-id': 'r34-njNloZw', title: '別再沈溺蜜月期！新婚小夫妻如何細水長流？', tag: '保險新手筆記' },
 		{ 'yt-id': 'vXe2tUs5r88', title: '別再靠北過日子！三明治族該怎麼規劃投保？', tag: '保險新手筆記' },
 		{ 'yt-id': 'vXe2tUs5r88', title: '別再靠北過日子！三明治族該怎麼規劃投保？', tag: '保險新手筆記' },
 		{ 'yt-id': 'vXe2tUs5r88', title: '別保了什麼都不知道！3招快速了解你的所有保單', tag: '保險新手筆記' },
